dcf4ad287fa030d456b761ea5d15afccfa62542b
Tetsuya <52840003+euro0707@users.noreply.github.com>
2019-10-05 23:02:27 +0900

    test

---
+++ main.js
@@ -0,0 +1,25 @@
+const lastName = '山田';
+const firstName = '太郎';
+const fullName = lastName + ' ' + firstName;
+const myAge = 10;
+const friendAge = 13;
+const totalAge = myAge + friendAge;
+const numberOFdays = totalAge * 365;
+const numberOFChildren = 3;
+const numberOFCakesByChild = numberOfCakes / numberOFChildren;
+const numberOfRestOfCakes = numberOFCakes % numberOFCakes;
+console.log(lastName);
+console.log(firstName);
+console.log(fullName);
+console.log(myAge);
+console.log(friendAge);
+console.log(totalAge);
+console.log(numberOFdays);
+console.log(numberOFChildren);
+console.log(numberOFCakes);
+console.log(numberOFCakesByChild);
+console.log(numberOfRestOfCakes);
+
+
+
+
